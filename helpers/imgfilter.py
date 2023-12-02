9fd5f7cb5df28e2e45cf14d42229702bc30daca6
aritramandal02 <aritra.mandal2002@gmail.com>
2023-12-02 21:33:14 +0530

    update

---
+++ helpers/imgfilter.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2 as cv
+from sklearn.cluster import KMeans
+
+darkBlue = (0, 51, 76)
+red = (217, 26, 33)
+lightBlue = (112, 150, 158)
+yellow = (252, 227, 166)
+
+
+def filter_obama(img_byte_arr):
+    img = np.asarray((img_byte_arr), dtype="uint8")
+    img = cv.imdecode(img, cv.IMREAD_COLOR)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if sum(img[i, j]) < 182:
+                img[i, j] = darkBlue
+            elif sum(img[i, j]) < 364:
+                img[i, j] = red
+            elif sum(img[i, j]) < 460:
+                img[i, j] = lightBlue
+            else:
+                img[i, j] = yellow
+    img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+    img = cv.imencode('.jpg', img)[1].tobytes()
+    return img
+
+
+def filter_gray():
+    pass
+
+
+image_filters = {
+    'obama': filter_obama,
+    'gray': filter_gray,
+}
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import logging
-from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, ConversationHandler
 from dotenv import load_dotenv
 import os
 from experiments import *
+from bot_handlers import *
 
 load_dotenv()
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
@@ -14,50 +13,6 @@ logging.basicConfig(
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
-
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
-    )
-
-
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Help!")
-
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(update.message.text)
-
-
-async def send_creater_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_document(document='https://python-telegram-bot.org/static/testfiles/telegram.gif')
-
-
-async def choose_filter(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if not context.args:
-        reply = "These are the filters I can apply:\n"
-        for filter in image_filters.keys():
-            reply += f'`/imgfilter {filter}`\n'
-        reply += "Click and copy any of the above filters and send to apply\."
-        await update.message.reply_text(reply, parse_mode='MarkdownV2')
-    if context.args:
-        if context.args[0] in image_filters.keys():
-            context.user_data['filter'] = context.args[0]
-            await update.message.reply_text(f"Ok send the image. {context.user_data['filter']} filter will be applied.")
-        else:
-            await update.message.reply_text(f"Sorry, I don't know about {context.args[0]} filter.")
-
-async def apply_filter(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if context.user_data.get('filter'):
-        image = await update.message.effective_attachment[-1].get_file()
-        await update.message.reply_text("Image Uploaded! Applying the filter. Please wait...")
-        img_arr = await image.download_as_bytearray()
-        processed_img = image_filters[context.user_data['filter']](img_arr)
-        await update.message.reply_photo(photo=processed_img)
-        del context.user_data['filter']
 
 
 def main() -> None:
